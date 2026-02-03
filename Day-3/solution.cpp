24d2cecc2bb1efd1a152bd03b7aa2a192606e027
Ansh Sharma <ansh.sharma12sep@gmail.com>
2026-02-03 15:48:40 +0530

    Added third file

---
+++ Day-3/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, original, reversed = 0;
+    cin >> n;
+
+    original = n;
+
+    while (n > 0) {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reversed)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
